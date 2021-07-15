dc2d72fb73b7494fc7310bd4dc318df3ac6e2cf1
SeongjunP <guppy217@naver.com>
2021-07-15 20:51:19 +0900

    d14

---
+++ java-basic/src/main/java/com/eomcs/algorithm/quiz/level4/Test2.java
@@ -0,0 +1,12 @@
+package com.eomcs.algorithm.quiz.level4;
+
+public class Test2 {
+
+  public static void main(String[] args) {
+    int a = 5 , b = 6;
+    System.out.println(a);
+    System.out.println(b);
+
+  }
+
+}
