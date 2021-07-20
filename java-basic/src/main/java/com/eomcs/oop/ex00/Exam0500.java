066d9893e510fcbfd9ff485e40abd3557ce69294
SeongjunP <guppy217@naver.com>
2021-07-20 20:08:48 +0900

    d16

---
+++ java-basic/src/main/java/com/eomcs/oop/ex00/Exam0500.java
@@ -0,0 +1,47 @@
+package com.eomcs.oop.ex00;
+
+public class Exam0500 {
+	  static class Calculator {
+		  int result = 0;
+		  
+		  void plus(int value) {
+			  this.result += value;
+		  }
+
+		  void minus(int value) {
+			  this.result -= value;
+		  }
+
+		  void multiple(int value) {
+			  this.result *= value;
+		  }
+
+		  void divide(int value) {
+			  this.result /= value;
+		  }
+	  }
+	 public static void main(String[] args) {
+		 Calculator c1 = new Calculator();
+		 Calculator c2 = new Calculator();
+		 
+		 c1.plus(2);
+		 c2.plus(3);
+		    
+		 c1.plus(3);
+		 c2.multiple(2);
+		    
+		 c1.minus(1);
+		 c2.plus(7);
+		    
+		 c1.multiple(7);
+		 c2.divide(4);
+		    
+		    c1.divide(3);
+		    c2.minus(5);
+
+		    System.out.printf("result = %d\n", c1.result);
+		    System.out.printf("result = %d\n", c2.result);
+		  }
+
+
+}
