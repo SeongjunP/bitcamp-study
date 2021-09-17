b8ee0ea85c1486b4187e9980e8065a197fd5e7ca
SeongjunP <guppy217@naver.com>
2021-09-17 14:54:43 +0900

    21/09/17 challenge update

--- java-basic/src/main/java/com/eomcs/basic/ex02/Exam0123.java
+++ java-basic/src/main/java/com/eomcs/basic/ex02/Exam0123.java
@@ -13,22 +13,20 @@ public class Exam0123 {
       this.name = name;
       this.age = age;
     }
-    
-    
+
     // String의 equals()처럼 내용이 같은지를 비교하도록 만들고 싶다면,
     // Object에서 상속 받은 메서드를 오버라이딩 하라.
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Member other = (Member) obj;
-		return age == other.age && Objects.equals(name, other.name);
-	}
-
-    
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Member other = (Member) obj;
+      return age == other.age && Objects.equals(name, other.name);
+    }
   }
 
   public static void main(String[] args) {
